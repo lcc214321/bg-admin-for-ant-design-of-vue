8047aaf8482b0d49ea73ae7fb9061211a4780057
282245889@qq.com <575088Wuling>
2019-10-11 21:10:53 +0800

    Initial commit

---
+++ bg-admin-for-ant-design-of-vue-web-core/src/main/java/com/bg/admin/BgAdminForAntDesignOfVueWebCoreApplication.java
@@ -0,0 +1,13 @@
+package com.bg.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgAdminForAntDesignOfVueWebCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgAdminForAntDesignOfVueWebCoreApplication.class, args);
+    }
+
+}
